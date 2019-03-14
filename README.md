8f134ce2174433fe5cd5775ef30cfe5fc8e7ec4b
KinoshitaYstr <photo_18@outlook.jp>
2019-03-14 19:34:20 +0900

    mnistの文字の読み取りテスト

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 * C言語で深層学習書いてみる
+
+## 参考サイト
+* http://kenjiwn.hatenablog.com/entry/2017/07/09/201524
+* http://yann.lecun.com/exdb/mnist/
+* https://marycore.jp/prog/c-lang/data-type-ranges-and-bit-byte-sizes/
--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char const *argv[])
-{
-    printf("hello world!\n");
+void testReadBinary();
+
+int main(int argc, char const *argv[]){
+    testReadBinary("dataset/mnist/t10k-images.idx3-ubyte");
+    testReadBinary("dataset/mnist/train-images.idx3-ubyte");
     return 0;
+}
+
+void testReadBinary(char fname[]){
+    FILE *fp;
+    unsigned char dataType[4];
+    char rows;
+    char cols;
+    unsigned char val;
+    char flag;
+    
+    fp = fopen(fname,"rb");
+    if(fp == NULL){
+        fputs("file read open error\n",stderr);
+        exit(-1);
+    }
+
+    fseek(fp,8+3,SEEK_SET);
+    fread(&rows,1,1,fp);
+    fseek(fp,12+3,SEEK_SET);
+    fread(&cols,1,1,fp);
+    printf("(rows,cols) = (%d,%d)\n",rows,cols);
+    
+    fseek(fp,16,SEEK_SET);
+    for(int c = 0;c < cols;c++){
+        for(int r = 0;r < rows;r++){
+            fread(&val,1,1,fp);
+            printf("%4d",val);
+        }
+        printf("\n");
+    }
+
+
+    fclose(fp);
 }
