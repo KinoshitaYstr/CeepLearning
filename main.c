853d373aa8b1dcd6ed09451b821f42e99a3f2172
KinoshitaYstr <photo_18@outlook.jp>
2019-05-04 16:14:54 +0900

    create neuron params and functions

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 typedef struct{
     unsigned char row;
@@ -13,13 +14,18 @@ typedef struct{
     double *v;
 }vector;
 
+typedef struct{
+    double bias;
+    double *weights;
+    unsigned int weights_size;
+}neuron_params;
+
 void testReadBinary();
 
 void printBit(unsigned int val,int size);
 unsigned int getMnistSize(FILE *fp);
 unsigned char getRows(FILE *fp);
 unsigned char getCols(FILE *fp);
-double** getMnistMatrix(FILE *fp,unsigned char rows,unsigned char cols,int num);
 void printDoubleMatrix(matrix m);
 void writeDoubleMatrix2IntInCSV(matrix m,char fname[]);
 void initMatrix(matrix *m,FILE *fp);
@@ -28,6 +34,8 @@ void initVector(vector *v,FILE *fp);
 void readMnistVector(vector *v,FILE *fp,int num);
 void exchangeMatrx2Vector(matrix m,vector *v);
 void exchangeVector2Matrix(vector v,matrix *m);
+void initNeuron(neuron_params *n,unsigned int size);
+double calcVectorNeuron(vector v,neuron_params n);
 
 int main(int argc, char const *argv[]){
     testReadBinary("dataset/mnist/t10k-images.idx3-ubyte");
@@ -77,7 +85,7 @@ unsigned char getCols(FILE *fp){
 void printDoubleMatrix(matrix m){
     for(int i = 0;i < m.row;i++){
         for(int j = 0;j < m.col;j++){
-            printf("%4d",(int)m.m[i][j]);
+            printf("%4d",(int)(m.m[i][j]*255));
         }
         printf("\n");
     }
@@ -85,13 +93,20 @@ void printDoubleMatrix(matrix m){
 
 void writeDoubleMatrix2IntInCSV(matrix m,char fname[]){
     FILE *fp;
+    int tmp;
     if((fp = fopen(fname,"w")) == NULL){
         printf("create file error\n");
         exit(-1);
     }
     for(int i = 0;i < m.row;i++){
-        for(int j = 0;j < m.col-1;j++) fprintf(fp,"%d,",(int)m.m[i][j]);
-        fprintf(fp,"%d\n",(int)m.m[i][m.col-1]);
+        for(int j = 0;j < m.col-1;j++){
+            tmp = m.m[i][j] <= 0.0 ? 0 : (int)(m.m[i][j]*255);
+            tmp = tmp > 255 ? 255 : tmp;
+            fprintf(fp,"%d,",tmp);
+        }
+        tmp = m.m[i][m.col-1] <= 0.0 ? 0 : (int)(m.m[i][m.col-1]*255);
+        tmp = tmp > 255 ? 255 : tmp;
+        fprintf(fp,"%d\n",tmp);
     }
     fclose(fp);
 }
@@ -109,7 +124,7 @@ void readMnistMatrix(matrix *m,FILE *fp,int num){
     for(int i = 0;i < m->row;i++){
         for(int j = 0;j < m->col;j++){
             fread(&tmp,1,1,fp);
-            m->m[i][j] = (double)tmp;
+            m->m[i][j] = (double)tmp/255.0;
         }
     }
 }
@@ -123,6 +138,7 @@ void testReadBinary(char fname[]){
     unsigned int fsize = 0;
     matrix testM;
     vector v;
+    neuron_params n;
 
     fp = fopen(fname,"rb");
     if(fp == NULL){
@@ -134,9 +150,11 @@ void testReadBinary(char fname[]){
     printf("fsize is %d\n",fsize);
     initMatrix(&testM,fp);
     initVector(&v,fp);
+    initNeuron(&n,v.size);
     for(int i = 0;i < 3;i++){
         readMnistMatrix(&testM,fp,i);
         exchangeMatrx2Vector(testM,&v);
+        printf("calc = %f\n",calcVectorNeuron(v,n));
         exchangeVector2Matrix(v,&testM);
         printDoubleMatrix(testM);
         writeDoubleMatrix2IntInCSV(testM,"result/test.csv");
@@ -158,7 +176,7 @@ void readMnistVector(vector *v,FILE *fp,int num){
     fseek(fp,16+num*v->size,SEEK_SET);
     for(int i = 0;i < v->size;i++){
         fread(&tmp,1,1,fp);
-        v->v[i] = (double)tmp;
+        v->v[i] = (double)tmp/255.0;
     }
 }
 
@@ -175,4 +193,22 @@ void exchangeVector2Matrix(vector v,matrix *m){
     for(int i = 0;i < m->row;i++)
         for(int j = 0;j < m->col;j++)
             m->m[i][j] = v.v[i*m->row+j];
+}
+
+void initNeuron(neuron_params *n,unsigned int size){
+    srand((unsigned)time(NULL));
+    n->weights_size = size;
+    n->weights = (double *)malloc(sizeof(double)*size);
+    for(int i = 0;i < size;i++) n->weights[i] = (double)rand()/RAND_MAX;
+    n->bias = (double)rand()/RAND_MAX;
+}
+
+double calcVectorNeuron(vector v,neuron_params n){
+    if(v.size != n.weights_size){
+        printf("vec and weight size error\n");
+        exit(-1);
+    }
+    double r = 0;
+    for(int i = 0;i < v.size;i++) r += v.v[i]*n.weights[i];
+    return r+n.bias;
 }
